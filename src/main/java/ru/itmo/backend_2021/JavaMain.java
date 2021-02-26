26b2c88bb6cfbe2c866189058b3ac6815ed3e233
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-02-26 16:44:02 +0200

    formatting

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -1,9 +1,5 @@
 package ru.itmo.backend_2021;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-
 public class JavaMain {
   public static void main(String[] args) {
     System.out.println("Hello Java");
