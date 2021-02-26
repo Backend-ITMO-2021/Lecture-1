0e07fdfd5f790429c6356b9fe87e3aba52cfa29a
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-02-26 16:43:06 +0200

    formatting

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -3,13 +3,13 @@ package ru.itmo.backend_2021;
 import scala.NotImplementedError;
 
 public class JavaMain {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+  }
 
-    public static int fib(int n) {
-        // Your code goes here
-        throw new NotImplementedError("");
-        // And ends there
-    }
+  public static int fib(int n) {
+    // Your code goes here
+    throw new NotImplementedError("");
+    // And ends there
+  }
 }
