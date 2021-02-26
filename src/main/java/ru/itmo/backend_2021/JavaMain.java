aa40a8cf91e8f2fe23d9622196dce7471978769a
SpaceOcean <komsomolenko11@gmail.com>
2021-02-26 23:55:49 +0300

    add lab_1

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -3,13 +3,21 @@ package ru.itmo.backend_2021;
 import scala.NotImplementedError;
 
 public class JavaMain {
-  public static void main(String[] args) {
-    System.out.println("Hello Java");
-  }
+    public static void main(String[] args) { }
 
-  public static int fib(int n) {
-    // Your code goes here
-    throw new NotImplementedError("");
-    // And ends there
-  }
+    public static long fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        long fir_element = 0;
+        long sec_element = 1;
+        long sum = 0;
+        for (int i = 1; i < n; i++) {
+            sum = fir_element + sec_element;
+            fir_element = sec_element;
+            sec_element = sum;
+        }
+
+        return sec_element;
+    }
 }
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -4,10 +4,19 @@ object ScalaMain {
   def main(args: Array[String]): Unit = {
     println("Hello Scala")
   }
-
   def fib(n: Int): Int = {
-    // Your code goes here
-    ???
-    // And ends there
+    if (n == 0) {
+      return 0
+    }
+    var fir_element: Int = 0
+    var sec_element: Int = 1
+    var sum: Int = 0
+    for (i <- 1 until n) {
+      sum = fir_element + sec_element
+      fir_element = sec_element
+      sec_element = sum
+    }
+
+    return sec_element
   }
 }
