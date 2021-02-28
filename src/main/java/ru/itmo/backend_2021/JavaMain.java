df016443112c13c2cf29753e08613ee4c57f70e4
IGNATII <mr.ignat2000@gmail.com>
2021-02-28 12:46:06 +0300

    Сделал 1 работу с дополнительными заданиями.

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -9,7 +9,25 @@ public class JavaMain {
 
   public static int fib(int n) {
     // Your code goes here
-    throw new NotImplementedError("");
+    
+  	/*
+    if (n <= 1) return n;
+    return fib(n-1) + fib(n-2);
+    */
+
+    if (n <= 1) return n;
+    int prev = 0;
+    int next = 1;
+    int s = 0;
+
+    for (int i=2; i<=n; i++) {
+    	s = prev+next;
+    	prev = next;
+    	next = s;
+    }
+    return s;
+
+    // throw new NotImplementedError("");
     // And ends there
   }
 }
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -7,7 +7,32 @@ object ScalaMain {
 
   def fib(n: Int): Int = {
     // Your code goes here
-    ???
+    
+    /*
+    n match {
+    	case 0 => 0
+    	case 1 => 1
+    	case n => fib(n-1) + fib(n-2)
+    }
+	*/
+
+	n match {
+		case 0 => 0
+		case 1 => 1
+		case n => {
+			var prev: Int = 0
+    		var next: Int = 1
+    		var s: Int = 0
+    		for (i<-2 to n) {
+    			s = prev + next
+    			prev = next
+    			next = s
+    		}
+    		return s
+		}
+	}
+
+    // ???
     // And ends there
   }
 }
