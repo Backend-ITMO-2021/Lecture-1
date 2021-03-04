e0ec1fc5938e643a8b1de6d53f1c48202f56fddf
Trang <trangology@gmail.com>
2021-03-04 13:21:58 +0300

    Add solutions

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -1,6 +1,5 @@
 package ru.itmo.backend_2021;
 
-import scala.NotImplementedError;
 
 public class JavaMain {
   public static void main(String[] args) {
@@ -8,8 +7,20 @@ public class JavaMain {
   }
 
   public static int fib(int n) {
-    // Your code goes here
-    throw new NotImplementedError("");
-    // And ends there
+    if (n < 0) {
+      throw new IllegalArgumentException("`n` CANNOT be a negative integer!");
+    }
+
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+
+    int f1 = 1;
+    int f2 = 1;
+    for (int i = 3; i <= n; i++) {
+      int f3 = f1 + f2;
+      f1 = f2;
+      f2 = f3;
+    }
+    return f2;
   }
 }
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -6,8 +6,18 @@ object ScalaMain {
   }
 
   def fib(n: Int): Int = {
-    // Your code goes here
-    ???
-    // And ends there
+    if (n < 0) throw new IllegalArgumentException("`n` CANNOT be a negative integer!")
+
+    if (n == 0) return 0
+    if (n == 1 || n == 2) return 1
+
+    var f1 = 1
+    var f2 = 1
+    for (i <- 3 to n) {
+      val f3 = f1 + f2
+      f1 = f2
+      f2 = f3
+    }
+    return f2
   }
 }
