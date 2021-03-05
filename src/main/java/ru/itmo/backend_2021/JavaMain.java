c302ddad6b6481bdfc9593d2a1b943b660bad986
Shubham Kunal <shubhamkunal19@gmail.com>
2021-03-05 22:36:30 +0530

    Tests 3 and 4 has also been passed

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -5,11 +5,24 @@ import scala.NotImplementedError;
 public class JavaMain {
   public static void main(String[] args) {
     System.out.println("Hello Java");
+    System.out.println(fib(10));
   }
-
+// SEQUENCE NUMBERING BEGINS FROM 1 AND NOT FROM 0. SO 1st NUMBER IS 0 AND NOT 1
   public static int fib(int n) {
     // Your code goes here
-    throw new NotImplementedError("");
+    int n1=0,n2=1,k=2,temp=0;
+    if(n==1){return 0;}
+    else if(n==2){return 1;}
+    else{
+     while (k!=n){
+       temp=n1+n2;
+       n1=n2;
+       n2=temp;
+       k++;
+     }
+     return n2;}
+    }
+   //throw new NotImplementedError("");
     // And ends there
   }
-}
+
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -3,11 +3,36 @@ package ru.itmo.backend_2021
 object ScalaMain {
   def main(args: Array[String]): Unit = {
     println("Hello Scala")
+    println(fib(10))
   }
 
+  // SEQUENCE NUMBERING BEGINS FROM 1 AND NOT FROM 0. SO 1st NUMBER IS 0 AND NOT 1
   def fib(n: Int): Int = {
     // Your code goes here
-    ???
-    // And ends there
+    //var n1: Int = 0; var n2: Int =1; var k: Int =2; var temp: Int =0
+    var n1: Int = 0
+    var n2: Int = 1
+    var k: Int = 2
+    var temp: Int = 0
+    if (n == 1) {
+      return 0
+    }
+    else {
+      if (n == 2) {
+        return 1
+      }
+      else {
+        while ( {
+          k != n
+        }) {
+          temp = n1 + n2
+          n1 = n2
+          n2 = temp
+          k += 1
+        }
+        return n2
+      }
+    }
+
   }
 }
