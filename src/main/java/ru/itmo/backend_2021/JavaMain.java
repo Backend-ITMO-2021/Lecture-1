7c90d41c0434677fb0de4761cc666d5a2e31c1a0
ShadowErm <shadow0touch@gmail.com>
2021-03-05 21:56:38 +0300

    add lad01

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -1,7 +1,5 @@
 package ru.itmo.backend_2021;
 
-import scala.NotImplementedError;
-
 public class JavaMain {
   public static void main(String[] args) {
     System.out.println("Hello Java");
@@ -9,7 +7,17 @@ public class JavaMain {
 
   public static int fib(int n) {
     // Your code goes here
-    throw new NotImplementedError("");
+    if (n == 0) {
+      return 0;
+    }
+    int number = 0;
+    int nextNumber = 1;
+    for (int i = 1; i < n; i++) {
+      int summa = nextNumber;
+      nextNumber = nextNumber + number;
+      number = summa;
+    }
+    return nextNumber;
     // And ends there
   }
 }
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -7,7 +7,15 @@ object ScalaMain {
 
   def fib(n: Int): Int = {
     // Your code goes here
-    ???
+        if (n == 0) return 0
+        var number = 0
+        var nextNumber = 1
+        for (_ <- 1 until n) {
+          val summa = nextNumber
+          nextNumber = nextNumber + number
+          number = summa
+        }
+        nextNumber
     // And ends there
   }
 }
