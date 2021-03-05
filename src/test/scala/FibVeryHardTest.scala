513dc0360ede6eb0577cf243b473ac0e1dc94b19
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-03-05 21:35:46 +0200

    fix tests

--- src/test/scala/FibVeryHardTest.scala
+++ src/test/scala/FibVeryHardTest.scala
@@ -1,16 +1,7 @@
 import org.scalatest.funsuite.AnyFunSuite
 import ru.itmo.backend_2021.{JavaMain, ScalaMain}
 
-class FibHardTest extends AnyFunSuite {
-
-  test("ScalaMain.fib hard test") {
-    ScalaMain.fib(1000000)
-  }
-
-  test("JavaMain.fib hard test") {
-    JavaMain.fib(1000000)
-  }
-
+class FibVeryHardTest extends AnyFunSuite {
   test("ScalaMain.fib very hard test") {
     assert(ScalaMain.fib(80) == 23416728348467685L)
   }
