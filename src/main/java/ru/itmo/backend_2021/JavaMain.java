8d14300d3995caa996ec74f084fa8e9dd0a63dec
TikhonovaElena <lenatika0@gmail.com>
2021-03-05 23:50:36 +0300

    K33401, Тихонова. Идея не ругается, тесты запущены не были.

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -1,6 +1,7 @@
 package ru.itmo.backend_2021;
-
-import scala.NotImplementedError;
+import java.lang.Math;
+import java.math.BigInteger;
+import java.math.BigDecimal;
 
 public class JavaMain {
   public static void main(String[] args) {
@@ -8,8 +9,23 @@ public class JavaMain {
   }
 
   public static int fib(int n) {
-    // Your code goes here
-    throw new NotImplementedError("");
-    // And ends there
+    // 47 число выходит за инт
+    final int fibIntLim = 46;
+    if (n <= fibIntLim) {
+      int[] fibArr = new int[n+1];
+      fibArr[0] = 0;
+      fibArr[1] = 1;
+      for (int index=2; index<=n; index++) {
+        fibArr[index] = fibArr[index - 1] + fibArr[index - 2];
+      }
+      return fibArr[n];
+    }
+    final double sqrtOfFive = Math.sqrt(5d);
+    final double phi = (1d + sqrtOfFive) / 2d;
+    // The length of Fibonacci sequence number on the position 1 000 000 is 208 988 digits.
+    // Is this ok for bigInteger? no, it's not.
+    // Но при этом ведь возвращаемое значение вообще должно быть int
+    final BigInteger result = BigDecimal.valueOf(Math.pow(phi, n) / sqrtOfFive).toBigInteger();
+    return 1000000;
   }
 }
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -6,8 +6,15 @@ object ScalaMain {
   }
 
   def fib(n: Int): Int = {
-    // Your code goes here
-    ???
-    // And ends there
+    var fibIntLim = 46;
+    if (n <= fibIntLim) {
+      var fibArr = new Array[int](n + 1);
+      fibArr[0] = 0;
+      fibArr[1] = 1;
+      for (index <- 2 to n) {
+        fibArr[index] = fibArr[index - 1] + fibArr[index - 2];
+      }
+      return fibArr[n];
+    } return 1000000;
   }
 }
