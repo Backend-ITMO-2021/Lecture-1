21cad89d144058b0ea0dba7407cba4e7bd47e0ed
Sergey Polyakov <sergpolyakow@mail.ru>
2021-05-25 15:28:00 +0300

    First working commit

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -9,7 +9,22 @@ public class JavaMain {
 
   public static long fib(int n) {
     // Your code goes here
-    throw new NotImplementedError("");
+
+    if (n < 0) {
+      throw new IndexOutOfBoundsException("Index number must be positive");
+    }
+
+    // +2 to process n == 0
+    long[] f = new long[n + 2];
+    f[0] = 0;
+    f[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      f[i] = f[i - 1] + f[i - 2];
+    }
+
+    return f[n];
+
     // And ends there
   }
 }
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -7,7 +7,20 @@ object ScalaMain {
 
   def fib(n: Int): Long = {
     // Your code goes here
-    ???
+
+    if (n < 0) {
+      new IndexOutOfBoundsException("Index number must be positive")
+    }
+
+    val f = new Array[Long](n + 2)
+    f(0) = 0
+    f(1) = 1
+
+    for (i <- 2 to n) {
+      f(i) = f(i - 1) + f(i - 2)
+    }
+
+    f(n)
     // And ends there
   }
 }
