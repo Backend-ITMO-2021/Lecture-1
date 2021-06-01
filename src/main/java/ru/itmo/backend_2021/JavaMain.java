cfda11dbd8f3b40cc7600ed99d6530e4dbf35277
FromItmo <FromItmo@ITMO.itmo>
2021-06-01 18:48:49 +0300

    Task 1 done

--- src/main/java/ru/itmo/backend_2021/JavaMain.java
+++ src/main/java/ru/itmo/backend_2021/JavaMain.java
@@ -4,12 +4,26 @@ import scala.NotImplementedError;
 
 public class JavaMain {
   public static void main(String[] args) {
-    System.out.println("Hello Java");
+    System.out.println(fib(80));
+
   }
 
   public static long fib(int n) {
     // Your code goes here
-    throw new NotImplementedError("");
-    // And ends there
+    if (n==0)
+    {
+      return 0;
+    }
+    long current = 1;
+    long prev = 0;
+    for (int i = 1; i < n; i++)
+    {
+      current = prev + current;
+      prev = current - prev;
+
+
+      // And ends there
+    }
+    return current;
   }
 }
--- src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
+++ src/main/scala/ru/itmo/backend_2021/ScalaMain.scala
@@ -7,7 +7,15 @@ object ScalaMain {
 
   def fib(n: Int): Long = {
     // Your code goes here
-    ???
+    if (n == 0) return 0
+    var current = 1
+    var prev = 0
+    for (i <- 1 until n) {
+      current = prev + current
+      prev = current - prev
+
+    }
+    return current
     // And ends there
   }
 }
